8ace8e0645dbac85019ca61a50208e3c30be1e1d
Killa <killa@ubuntu.(none)>
2013-08-16 17:04:23 +0200

    Set up an initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobMall::Application.config.secret_key_base = '1df6d9671575deacb0708978525ccfe55873b56dce9faad354b8472fa368d2ecbf4ae8089a4536e13993bef08742ff1800219d7a05c8a0c6fa62ebb812ae75c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MobMall::Application.config.session_store :cookie_store, key: '_MobMall_session'
