6ec9fc9bf8fd31513c99d441a859a5a001b52937
Killa <killa@ubuntu.(none)>
2013-08-17 18:35:42 +0200

    added sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,15 @@ MobMall::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   config.action_mailer.default_url_options = { :host => "http://mobmall.herokuapp.com/" }
 end
