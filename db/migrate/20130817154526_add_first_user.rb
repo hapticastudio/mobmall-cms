63c9804d51884fbdde48d9c6a9a3bcfa1de9079e
Killa <killa@ubuntu.(none)>
2013-08-17 17:50:18 +0200

    Added admin role to first user

--- db/migrate/20130817154526_add_first_user.rb
+++ db/migrate/20130817154526_add_first_user.rb
@@ -1,6 +1,6 @@
 class AddFirstUser < ActiveRecord::Migration
   def up
-    User.create(email: "l.strzebinczyk@gorailsgo.com", password: "secret", password_confirmation: "secret")
+    User.create(email: "l.strzebinczyk@gorailsgo.com", password: "secret", password_confirmation: "secret", role: 'admin')
   end
 
   def down
