ec01c54dcfd4c14238260c757df6e613e1fcaa09
Killa <killa@ubuntu.(none)>
2013-08-17 18:43:26 +0200

    change mail message to new users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,13 @@ class UserMailer < ActionMailer::Base
   def reset_password_email(user)
     @user = user
     @url  = edit_password_reset_url(user.reset_password_token)
-    mail(:to => user.email, :subject => "Your password has been reset")
+    
+    if @user.password_present?
+      subject = "Your password has been reset"
+    else
+      subject = "Welcome to MobMall!"
+    end
+
+    mail(:to => user.email, subject: subject)
   end
 end
