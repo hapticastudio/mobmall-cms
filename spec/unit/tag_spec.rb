5c6241c16c949c9ee405e902cf35a49496469f00
Killa <killa@ubuntu.(none)>
2013-08-23 23:31:02 +0200

    Ensured model updating on adding/removing relations

--- spec/unit/tag_spec.rb
+++ spec/unit/tag_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe Tag do
   it { should have_many :locals }
+
+  it "should be updated when tagging is added" do
+    tag = FactoryGirl.create(:tag, updated_at: 2.days.ago)
+    local = FactoryGirl.create(:local)
+    local.tag_list = tag.name
+    local.save
+    (tag.reload.updated_at - Time.now).abs.should < 1
+  end
+
+  it "should be updated when tagging is removed", focus: true do
+    local = FactoryGirl.create(:local)
+    tag = FactoryGirl.create(:tag, locals: [local])
+    tag_to_be_updated = FactoryGirl.create(:tag, updated_at: 2.days.ago, locals: [local])
+
+    local.tag_list = tag.name
+
+    (tag_to_be_updated.reload.updated_at - Time.now).abs.should < 1
+  end
 end
