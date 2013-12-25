889f152bad9b5c55ba8547f7aa4b49ae919df432
Łukasz Strzebińczyk <luki1989@tlen.pl>
2013-12-25 04:39:50 +0100

    I have no idea what I just added

--- app/controllers/moderator/events_controller.rb
+++ app/controllers/moderator/events_controller.rb
@@ -1,50 +1,52 @@
-class EventsController < ApplicationController
-  before_filter :require_login
-  before_filter :require_moderator
+module Moderator
+    class EventsController < ApplicationController
+      before_filter :require_login
+      before_filter :require_moderator
 
-  def new
-    @event = local.events.new
-  end
-
-  def create
-    @event = local.events.new(event_params)
-
-    if @event.save
-      redirect_to local_path(local)
-    else
-      render :new
+    def new
+      @event = local.events.new
     end
-  end
 
-  def edit
-    @event = local.events.where(id_params).first
-  end
+    def create
+      @event = local.events.new(event_params)
 
-  def update
-    @event = local.events.where(id_params).first
-
-    if @event.update_attributes(event_params)
-      redirect_to local_path(local)
-    else
-      render :edit
+      if @event.save
+        redirect_to local_path(local)
+      else
+        render :new
+      end
     end
-  end
 
-  private
+    def edit
+      @event = local.events.where(id_params).first
+    end
 
-  def event_params
-    params.require(:event).permit(:description, :begin_time, :end_time, :name, :short_description)
-  end
+    def update
+      @event = local.events.where(id_params).first
 
-  def local
-    @local ||= Local.where(id: local_id).first
-  end
+      if @event.update_attributes(event_params)
+        redirect_to local_path(local)
+      else
+        render :edit
+      end
+    end
 
-  def local_id
-    params.permit(:local_id)[:local_id]
-  end
+    private
 
-  def require_moderator
-    not_authorised unless local.moderator == current_user
+    def event_params
+      params.require(:event).permit(:description, :begin_time, :end_time, :name, :short_description)
+    end
+
+    def local
+      @local ||= Local.where(id: local_id).first
+    end
+
+    def local_id
+      params.permit(:local_id)[:local_id]
+    end
+
+    def require_moderator
+      not_authorised unless local.moderator == current_user
+    end
   end
 end
