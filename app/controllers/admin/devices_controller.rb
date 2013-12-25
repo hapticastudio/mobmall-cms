15e17acc5a13cad8437492eb82da091db4570d02
Łukasz Strzebińczyk <luki1989@tlen.pl>
2013-12-25 04:17:22 +0100

    fixed intendation

--- app/controllers/admin/devices_controller.rb
+++ app/controllers/admin/devices_controller.rb
@@ -1,15 +1,15 @@
 module Admin
-	class DevicesController < ApplicationController
-	  before_filter :require_login
-	  before_filter :require_admin
+  class DevicesController < ApplicationController
+    before_filter :require_login
+    before_filter :require_admin
 
-	  def index
-	    @devices = Device.all
-	  end
+    def index
+      @devices = Device.all
+    end
 
-	  def destroy
-	    Device.destroy_all
-	    redirect_to admin_devices_path
-	  end
-	end
+    def destroy
+      Device.destroy_all
+      redirect_to admin_devices_path
+    end
+  end
 end
